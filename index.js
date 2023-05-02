393b0a18b09c583daf37a7c743c5d9ce63629509
shojol021 <shojol021@gmail.com>
2023-05-02 13:49:03 +0600

    json data showed on the local server dynamically

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 
+const data = require('./data.json')
+// const cors = require('cors')
+
 app.get('/', (req, res) => {
     res.send('hello world')
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(data.chefs)
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const chefDetails = data.chefs.find(chef => chef.id === id)
+    res.send(chefDetails)
 })
 
 app.listen(port)
